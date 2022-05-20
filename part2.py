8869508c3ddd538a650527d1115328e8c6782650
veryanjohnson <105072451+veryanjohnson@users.noreply.github.com>
2022-05-20 13:14:08 -0400

    Update part2.py

--- part2.py
+++ part2.py
@@ -10,6 +10,6 @@ number in the formatted sentence that's already there.
 '''
 
 #code starts here
-number = input(int("Enter a number: ")
+number = int(input("Enter a number: "))
 print("Ten times your number is", 10 * number)
 print("Ninety-nine more than your number is", 99 + number)
