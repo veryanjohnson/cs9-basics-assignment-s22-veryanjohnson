069064cf620557f5038545866b7aed46f9e08141
veryanjohnson <105072451+veryanjohnson@users.noreply.github.com>
2022-05-19 21:46:03 -0400

    Update part3.py

--- part3.py
+++ part3.py
@@ -10,5 +10,5 @@ There are (at least) 6 errors in this code. Fix them so that it runs properly.
 number1 = input("Enter a number: ")
 number2 = int(input("Enter another number: ")
 
-print "The sum of your numbers is", number1 + Number2
-print(Seven times your second number is, 7(number2))
+print("The sum of your numbers is", number1 + number2)
+print("Seven times your second number is", 7 * number2)
